da557b6552a5964ee212be8dc01b688e5b2e3a41
Florian <floriandupuis1996@gmail.com>
2020-09-08 18:00:20 +0200

    Add JWT Middleware

--- server/routes/jwtAuth.js
+++ server/routes/jwtAuth.js
@@ -2,9 +2,10 @@ const router = require("express").Router();
 const pool = require("../db");
 const bcrypt = require("bcrypt");
 const jwtGenerator = require("../utils/jwtGenerator");
+const validInfo = require("../middleware/validInfo");
 //registering
 
-router.post("/register", async (req, res) => {
+router.post("/register", validInfo, async (req, res) => {
   try {
     //1. desctructure the req.body (name, email, password)
     const { name, email, password } = req.body;
@@ -36,6 +37,36 @@ router.post("/register", async (req, res) => {
   } catch (err) {
     console.log(err.message);
     res.status(500).send("Server Error");
+  }
+});
+
+//login route
+
+router.post("/login", validInfo, async (req, res) => {
+  try {
+    //1. destructure the req.body
+    const { email, password } = req.body;
+    //2. check if user doesn't exist (if not then we throw error)
+    const user = await pool.query("SELECT * FROM users WHERE user_email = $1", [
+      email,
+    ]);
+
+    if (user.rows.length === 0) {
+      return res.status(401).json("Password or Email is invalid");
+    }
+    //3. check if incoming password is the same the database password
+    const validPassword = await bcrypt.compare(
+      password,
+      user.rows[0].user_password
+    );
+    if (!validPassword) {
+      return res.status(401).json("Password or Email is invalid");
+    }
+    //4. give them the jwt token
+    const token = jwtGenerator(user.rows[0].user_id);
+    res.json({ token });
+  } catch (err) {
+    console.log(err.message);
   }
 });
 
