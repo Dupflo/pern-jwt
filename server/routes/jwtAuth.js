466dc21739f26428331a7fe3c6288cfa88e768c4
Florian <floriandupuis1996@gmail.com>
2020-09-08 18:28:40 +0200

    Build private routes

--- server/routes/jwtAuth.js
+++ server/routes/jwtAuth.js
@@ -3,6 +3,7 @@ const pool = require("../db");
 const bcrypt = require("bcrypt");
 const jwtGenerator = require("../utils/jwtGenerator");
 const validInfo = require("../middleware/validInfo");
+const authorization = require("../middleware/authorization");
 //registering
 
 router.post("/register", validInfo, async (req, res) => {
@@ -67,6 +68,15 @@ router.post("/login", validInfo, async (req, res) => {
     res.json({ token });
   } catch (err) {
     console.log(err.message);
+  }
+});
+
+router.get("/is-verify", authorization, async (req, res) => {
+  try {
+    res.json(true);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
   }
 });
 
