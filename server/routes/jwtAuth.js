722e38cd7b193650adf141cbeb2f3f35e94d8b9c
Florian <floriandupuis1996@gmail.com>
2020-09-08 17:20:32 +0200

    Create credentials on server side

---
+++ server/routes/jwtAuth.js
@@ -0,0 +1,39 @@
+const router = require("express").Router();
+const pool = require("../db");
+const bcrypt = require("bcrypt");
+//registering
+
+router.post("/register", async (req, res) => {
+  try {
+    //1. desctructure the req.body (name, email, password)
+    const { name, email, password } = req.body;
+    //2. check if user exist (if user exist then thrown error)
+    const user = await pool.query("SELECT * FROM users WHERE user_email = $1", [
+      email,
+    ]);
+
+    if (user.rows.length !== 0) {
+      return res.status(401).send("User already exist");
+    }
+    //3. Bcrypt the user password
+    const saltRound = 10;
+    const salt = await bcrypt.genSalt(saltRound);
+
+    const bcryptPassword = await bcrypt.hash(password, salt);
+    //4. Enter the new user inside our database
+
+    const newUser = await pool.query(
+      "INSERT INTO users (user_name, user_email, user_password) VALUES ($1, $2, $3) RETURNING *",
+      [name, email, bcryptPassword]
+    );
+
+    res.json(newUser.rows[0]);
+
+    //5. Generating our jwt token
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+module.exports = router;
